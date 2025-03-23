2deb666450b0ec3f6469a787c9102716c15f2f73
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2025-03-24 01:31:44 +0530

    make it easier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,7 @@ import { RouterOutlet } from '@angular/router';
 // we cannot define the variable directly in class 
 export class AppComponent {
   title = 'Hello jii';
-  // example let x=2;
-
-  // this function called when click Event Occur 
-  handleClickEvent(){
-    alert("yoou click that Button")
-    //call the method of own class
-    this.demoFun();
-  }
-  demoFun(){
-    console.log("Hii Angular js")
-  }
+  
 
 
   
