fbf36f6db777436fe935e69065ecf73ceb93c600
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2025-03-23 10:55:53 +0530

    deep in Click Event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { ProfileComponent } from './porfile/profile.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,LoginComponent,SignupComponent,ProfileComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,8 +13,16 @@ export class AppComponent {
   title = 'Hello jii';
   // example let x=2;
 
-  // this function called when click Event Occur i.e means if button is pressed
+  // this function called when click Event Occur 
   handleClickEvent(){
     alert("yoou click that Button")
+    //call the method of own class
+    this.demoFun();
   }
+  demoFun(){
+    console.log("Hii Angular js")
+  }
+
+
+  
 }
