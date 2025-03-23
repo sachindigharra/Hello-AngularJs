596b1c4de2926442675185e3c9afb1b8ff1853ac
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2025-03-24 00:58:10 +0530

    basic Event in AngularJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,25 +4,31 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 // we cannot define the variable directly in class 
 export class AppComponent {
   title = 'Hello jii';
-  // example let x=2;
-
-  // this function called when click Event Occur 
-  handleClickEvent(){
-    alert("yoou click that Button")
-    //call the method of own class
-    this.demoFun();
+  handleClick(event:any){
+    // Same As E is Object of Exception Java
+    console.log(event)
+    console.log(event.target.name)
+    console.log(event.target.classList)
   }
-  demoFun(){
-    console.log("Hii Angular js")
-  }
+  // Handle Event given Info About Event In console
 
+  handleEvent(event:MouseEvent){
+
+    console.log(event.type);
+    console.log()
+  }
+  handleChangeEvent(event:any){
+    
+    console.log(event.type)
+    console.log((event.target as HTMLInputElement).value)
+  }
 
   
 }
